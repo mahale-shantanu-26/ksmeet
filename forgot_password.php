649208e84ad31e8e5e9663c76a8784de599cc84b
mahale-shantanu-26 <mahaleshantanu3@gmail.com>
2020-09-02 17:37:31 +0530

    new commit

--- forgot_password.php
+++ forgot_password.php
@@ -22,7 +22,7 @@ if(isset($_POST['forgot'])){
             $mail->Host = 'smtp.gmail.com';             // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                     // Enable SMTP authentication
             $mail->Username = 'oscorper@gmail.com';          // SMTP username
-            $mail->Password = 'ThisIsMyNewPassword'; // SMTP password
+            $mail->Password = ''; // SMTP password
             $mail->SMTPSecure = 'tls';                  // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 587;                          // TCP port to connect to
 
